d3aafc84cc174ef7fcfc894bea65221189b5b92a
Maria Serna <mafesernaarboleda@gmail.com>
2017-12-16 22:06:06 -0500

    first commit

---
+++ src/app/utils/config.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs/Rx';
+import { Response } from '@angular/http';
+
+export class Config {
+
+  public static get API(): string {
+    return 'https://arcane-earth-62779.herokuapp.com/cohort';
+  }
+
+  public static extractData(res: Response) {
+    const body = res.json() || {};
+    return body;
+  }
+
+  public static handleError(error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    return Observable.throw(errMsg);
+  }
+
+}
