0827baeb9b9750af161f9adfe2babbfe01f39a55
Maria Serna <mafesernaarboleda@gmail.com>
2017-12-18 15:18:43 -0500

    fixed url

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -4,7 +4,7 @@ import { Response } from '@angular/http';
 export class Config {
 
   public static get API(): string {
-    return 'http://107.170.76.230:3000/cohort';
+    return 'https://107.170.76.230:3000/cohort';
   }
 
   public static extractData(res: Response) {
